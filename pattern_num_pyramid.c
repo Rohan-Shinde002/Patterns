2cd24c7c77dd7ec1a0239a571ae6f98298b2be21
Rohan <rohanshinde0022@gmail.com>
2021-08-24 08:21:41 +0530

    Number Pyramid 2

---
+++ pattern_num_pyramid.c
@@ -0,0 +1,32 @@
+/*PROGRAM TO PRINT FOLLOWING PATTERN
+
+     1 
+    2 2 
+   3 3 3 
+  4 4 4 4 
+ 5 5 5 5 5 
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k=5,m;
+    for(i=1; i<=5; i++)
+    {
+        for(j=1; j<=k; j++)
+        {
+            printf(" ");
+        }
+        for(m=1; m<=i; m++)
+        {
+            printf("%d",i);
+            printf(" ");
+        }
+        printf("\n");
+        k=k-1;
+    }
+      
+    return 0;
+}
+
