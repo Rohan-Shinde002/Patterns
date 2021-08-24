f6eb0afc7f56a8ffd39b80ae606eb8fd47e053cd
Rohan <rohanshinde0022@gmail.com>
2021-08-24 08:25:50 +0530

    Pyramid Pattern

---
+++ pyramid_pattern.c
@@ -0,0 +1,32 @@
+
+//PROGRAM TO PRINT PYRAMID PATTERN
+
+#include<stdio.h>
+
+int  main ()
+{
+  int n, i, j, k;
+  n=5;
+  for (i=0;i<n;i++)
+    {
+      for (j=0;j<=n - 1-i; j++)
+	    {  
+	       printf (" ");	       
+	    }
+    printf ("/");
+        for(k=0;k<i;k++)
+        {
+            if(i==n-1)
+            {
+            	printf("__");
+			}else
+			{
+				printf ("  ");
+			}
+			
+        }
+    printf ("\\ \n");
+    }
+
+  return 0;
+}
